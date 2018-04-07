21c2ee0f84287ea8b092d1590c264e07a6ee733b
小煤球 <1114570431@qq.com>
2018-04-07 16:57:39 +0800

    song play
    歌曲播放页

--- src/components/songplay/songplay.vue
+++ src/components/songplay/songplay.vue
@@ -3,10 +3,10 @@
         <!-- 歌曲播放页面 -->
         <div class="l">
             <div class="top-box">
-                <h3 class="title">qq音乐</h3>
+                <h3 class="title">qq音乐 <i class="iconfont icon-downforward"></i></h3>
                 <div class="needle-container">
                     <!-- 播放指针 -->
-                    <img src="./../../assets/needle.png" alt="">
+                    <img width="68" height="115" src="./../../assets/needle.png" alt="">
                 </div>
             </div>
             <!-- 播放的旋转的图片 -->
@@ -28,7 +28,21 @@
                 </div>
             </div>
         </div>
-        <div class="isPlay"></div>
+        <div class="isPlay">
+            <ul>
+                <!-- <li>顺序</li> -->
+                <li class="pre-song">
+                    <i class="iconfont icon-047caozuo_shangyishou"></i>
+                </li>
+                <li class="play-pause">
+                    <i class="iconfont icon-bofang"></i>
+                </li>
+                <li class="next-song">
+                    <i class="iconfont icon-49xiayishou"></i>
+                </li>
+            </ul>
+        </div>
+        <audio id="audio" src=""></audio>
     </div>
 </template>
 
@@ -37,15 +51,71 @@ export default {};
 </script>
 
 <style scoped lang="less">
-    .song-play{
-        position: fixed;
-        top:0;
-        left: 0;
-        bottom:0;
-        right:0;
-        background-color:#000;
-        color:#fff;
-        z-index:100;
+.song-play {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: #000;
+  color: #fff;
+  z-index: 100;
+  display: none;
+  .title {
+    text-align: center;
+    background-color: #31c27c;
+    height: 40px;
+    line-height: 40px;
+    position: relative;
+    font-size: 16px;
+    .iconfont {
+      position: absolute;
+      left: 10px;
+      font-size: 24px;
     }
-
+  }
+  .needle-container {
+    position: absolute;
+    top: 40px;
+    right: 33%;
+    width: 30px;
+    height: 60px;
+    z-index: 20;
+  }
+  .song-img {
+    width: 230px;
+    height: 230px;
+    border-radius: 50%;
+    background: #555;
+    position: absolute;
+    top: 110px;
+    left: 50%;
+    margin-left: -115px;
+  }
+  .lyrics{
+          position: absolute;
+    bottom: 100px;
+    text-align: center;
+    width: 100%;
+    line-height: 30px;
+  }
+  .isPlay{
+      position: absolute;
+      bottom:0;
+      left: 0;
+      width: 100%;
+      background-color: #333;
+      color:#fff;
+      height: 40px;
+      line-height: 44px;
+      text-align: center;;
+      li{
+          float: left;
+          width:33%;
+      }
+      .iconfont{
+          font-size: 20px;
+      }
+  }
+}
 </style>
